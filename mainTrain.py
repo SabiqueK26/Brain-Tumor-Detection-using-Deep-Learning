f8d85a81a0b84b5a7f07d9c62773d901be4de117
sabiquekhan <sabiqueislamkhan@gmail.com>
2022-08-07 23:42:06 +0600

    Model Building Done

--- mainTrain.py
+++ mainTrain.py
@@ -8,6 +8,7 @@ from sklearn.model_selection import train_test_split
 from keras.utils import normalize
 from keras.models import Sequential
 from keras.layers import Conv2D, MaxPooling2D, Activation, Dropout, Flatten, Dense
+from keras.utils import to_categorical
 
 image_directory='datasets/'
 
@@ -15,6 +16,7 @@ no_tumor_images = os.listdir(image_directory + 'no/')
 yes_tumor_images = os.listdir(image_directory + 'yes/')
 dataset = []
 label = []
+INPUT_SIZE = 64
 # print(no_tumor_images)
 # print(yes_tumor_images)
 
@@ -25,7 +27,7 @@ for i, image_name in enumerate(no_tumor_images):
     if(image_name.split('.')[1] == 'jpg'):
         image = cv2.imread(image_directory + 'no/' + image_name)
         image = Image.fromarray(image, 'RGB')
-        image = image.resize((64,64))
+        image = image.resize((INPUT_SIZE,INPUT_SIZE))
         dataset.append(np.array(image))
         label.append(0)
         
@@ -33,11 +35,11 @@ for i, image_name in enumerate(yes_tumor_images):
     if(image_name.split('.')[1] == 'jpg'):
         image = cv2.imread(image_directory + 'yes/' + image_name)
         image = Image.fromarray(image, 'RGB')
-        image = image.resize((64,64))
+        image = image.resize((INPUT_SIZE,INPUT_SIZE))
         dataset.append(np.array(image))
         label.append(1)
         
-# converting dataset into an numpy array
+# Converting Dataset into a numpy array
 dataset = np.array(dataset)
 label = np.array(label)
 
@@ -50,4 +52,36 @@ x_train, x_test, y_train, y_test = train_test_split(dataset, label, test_size=0.
 # print(y_test.shape)
 
 x_train = normalize(x_train, axis=1)
-x_test =normalize(x_test, axis=1)
+x_test = normalize(x_test, axis=1)
+
+y_train = to_categorical(y_train, num_classes=2)
+y_test = to_categorical(y_test, num_classes=2)
+ 
+# Build The Model
+model = Sequential()
+
+model.add(Conv2D(32, (3,3), input_shape=(INPUT_SIZE, INPUT_SIZE, 3)))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2,2)))
+
+model.add(Conv2D(32, (3,3), kernel_initializer='he_uniform'))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2,2)))
+
+model.add(Conv2D(64, (3,3), kernel_initializer='he_uniform'))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2,2)))
+
+model.add(Flatten())
+model.add(Dense(64))
+model.add(Activation('relu'))
+model.add(Dropout(0.5))
+model.add(Dense(2))
+model.add(Activation('softmax'))
+
+model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+# Model Fitting
+model.fit(x_train, y_train, batch_size=26, verbose=1, epochs=10, validation_data=(x_test,y_test), shuffle=False)
+
+model.save('BrainTumor10EpocsCategorical.h5')
